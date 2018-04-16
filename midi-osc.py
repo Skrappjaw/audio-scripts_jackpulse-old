d7a234372099befa118af2a0cf8d66a888e7a68b
Skrappjaw <skrappjaw@gmail.com>
2018-04-16 18:50:59 -0400

    Add files via upload

---
+++ midi-osc.py
@@ -0,0 +1,49 @@
+# Get the mididings and OSC stuffs
+from mididings import *
+from mididings.extra.osc import SendOSC
+from mididings.extra.osc import OSCInterface
+hook(OSCInterface())
+
+# Set up the OSC port 
+#(you define it in non-mixer thanks to  "--osc-port=15159" option - no quotes)
+port =  15159
+ 
+#The actual conversion stuff
+run(
+   Filter(CTRL) >> CtrlSplit({
+       # Non-mixer maping
+       #It's always : /strip/[strip_name]/[effect_name]/[control_name]
+       #Non-mixer uses values from 0.0 to 1.0. Therfore you have to divide by 127
+       # Caution : 127.0 ( .0 !!). Check 'python's promotion' if you want to know why.
+
+       #Pan Knobs for sends and sub mixes
+       16: SendOSC(port, '/strip/VoipSend/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+
+
+       #Fader Volumes
+       1: SendOSC(port, '/strip/UMC-1/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       2: SendOSC(port, '/strip/UMC-2/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       3: SendOSC(port, '/strip/UMC-3/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       4: SendOSC(port, '/strip/UMC-4/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       5: SendOSC(port, '/strip/Game/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       6: SendOSC(port, '/strip/Music/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       7: SendOSC(port, '/strip/Voip/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+       8: SendOSC(port, '/strip/System/Gain/Gain%20(dB)', lambda ev: ev.value / 127.0),
+
+       # Mute Buttons
+       48: SendOSC(port, '/strip/UMC-1/Gain/Mute', lambda ev: ev.value / 127.0),
+       49: SendOSC(port, '/strip/UMC-2/Gain/Mute', lambda ev: ev.value / 127.0),
+       50: SendOSC(port, '/strip/UMC-3/Gain/Mute', lambda ev: ev.value / 127.0),
+       51: SendOSC(port, '/strip/UMC-4/Gain/Mute', lambda ev: ev.value / 127.0),
+       52: SendOSC(port, '/strip/Game/Gain/Mute', lambda ev: ev.value / 127.0),
+       53: SendOSC(port, '/strip/Music/Gain/Mute', lambda ev: ev.value / 127.0),
+       54: SendOSC(port, '/strip/Voip/Gain/Mute', lambda ev: ev.value / 127.0),
+       55: SendOSC(port, '/strip/System/Gain/Mute', lambda ev: ev.value / 127.0),
+       32: SendOSC(port, '/strip/VoipSend/Gain/Mute', lambda ev: ev.value / 127.0),
+
+
+
+
+
+   })
+)
---
+++ start_jack.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+killall -9 non-mixer
+killall -9 carla
+killall -9 jackdbus
+sleep 5
+jack_control start & 
+sleep 1
+pulseaudio --start
+cadence &
+sleep 5 
+pacmd unload-module module-jack-sink
+pacmd unload-module module-jack-source
+sleep 5
+
+#pulseaudio bridges | in / out
+
+pacmd load-module module-jack-source channels=2 source_name=voip-in client_name=voip-in connect=false
+pacmd load-module module-jack-sink channels=2 sink_name=voip-out client_name=voip-out connect=false
+
+pacmd load-module module-jack-sink channels=2 sink_name=games-out client_name=games-out connect=false
+
+pacmd load-module module-jack-source channels=2 source_name=media-in client_name=media-in connect=false
+pacmd load-module module-jack-sink channels=2 sink_name=media-out client_name=media-out connect=false
+
+
+sleep 2
+a2jmidid -e &
+sleep 2
+killall mididings
+mididings -f ~/audio-config/midi-osc.py &
+sleep 2
+non-mixer --osc-port=15159 ~/audio-config/non.mix &
+sleep 2
+
+sleep 2
+
+carla ~/audio-config/carla.presets/default.carxp &
+sleep 3
+wall "Audio systems up and running"
+sleep 1
+exit 0
